23e6279e883ee493aecfce0ffe6fe4c7d7ae0900
isitaaa <143053705+isitaaa@users.noreply.github.com>
2024-11-12 21:35:54 -0500

    Create condicional anidado con operadores lógicos.c

---
+++ condicional anidado con operadores lógicos.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int x = 4, y = 8;
+    if (x < y && x > 2) {
+        if (y < 10) {
+            printf("x es menor que y y mayor que 2, y y es menor que 10.\n");
+        }
+    }
+    return 0;
+}
