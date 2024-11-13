5a4f4e217e35dca134aec01ca9b32d0d79cae687
isitaaa <143053705+isitaaa@users.noreply.github.com>
2024-11-12 21:33:46 -0500

    Create if anidado en.c

---
+++ if anidado en.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int x = 5, y = 10;
+    if (x < y) {
+        if (x > 3) {
+            printf("x es menor que y y mayor que 3.\n");
+        }
+    }
+    return 0;
+}
